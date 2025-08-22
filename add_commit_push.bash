e09830541d0f56fa868011fbbb0b0a358e53789e
ed <jiangfei419@gmail.com>
2025-08-22 18:50:57 +0800

    20250822

--- add_commit_push.bash
+++ add_commit_push.bash
@@ -7,7 +7,6 @@ cp ~/grace_20190205/historyOfSalvation/BT_ALL ./
 INFO=$(date +%Y%m%d)
 
 
-export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
 
 #git status
 
